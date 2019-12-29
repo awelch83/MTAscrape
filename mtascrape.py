6b21b062b2d7845e59aff64ff03d375c553e3cac
Aaron Welch <acw036@Aarons-MacBook-Pro.local>
2019-12-29 00:46:53 -0600

    Add loop to retrieve all data files

--- mtascrape.py
+++ mtascrape.py
@@ -7,9 +7,9 @@ url = 'http://web.mta.info/developers/turnstile.html'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-current_atag = soup.findAll('a')[35]
-link = current_atag['href']
-url_download = 'http://web.mta.info/developers/' + link
-
-urllib.request.urlretrieve(url_download,'./turnstile/'+link[link.find('/turnstile_')+1:])
-
+for i in range(35,len(soup.findAll('a'))+1):
+    current_atag = soup.findAll('a')[35]
+    link = current_atag['href']
+    url_download = 'http://web.mta.info/developers/' + link
+    urllib.request.urlretrieve(url_download,'./turnstile/'+link[link.find('/turnstile_')+1:])
+    time.sleep(1)
