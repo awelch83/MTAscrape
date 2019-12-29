d3ccfcb61660d5a852817c6c281c7b67a554ab4e
Aaron Welch <acw036@Aarons-MacBook-Pro.local>
2019-12-29 00:47:41 -0600

    Fixed silly logic error.

--- mtascrape.py
+++ mtascrape.py
@@ -8,7 +8,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 for i in range(35,len(soup.findAll('a'))+1):
-    current_atag = soup.findAll('a')[35]
+    current_atag = soup.findAll('a')[i]
     link = current_atag['href']
     url_download = 'http://web.mta.info/developers/' + link
     urllib.request.urlretrieve(url_download,'./turnstile/'+link[link.find('/turnstile_')+1:])
