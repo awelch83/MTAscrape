182d5e4987bd11ccd7ead919a0fb35d1d74060f1
Aaron Welch <acw036@Aarons-MacBook-Pro.local>
2019-12-29 00:09:05 -0600

    Successfully retrieving one text file.

--- mtascrape.py
+++ mtascrape.py
@@ -5,4 +5,11 @@ from bs4 import BeautifulSoup
 
 url = 'http://web.mta.info/developers/turnstile.html'
 response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+current_atag = soup.findAll('a')[35]
+link = current_atag['href']
+url_download = 'http://web.mta.info/developers/' + link
+
+urllib.request.urlretrieve(url_download,'./'+link[link.find('/turnstile_')+1:])
 
